3e60b040224b8c8b2ae12ac14cbd73d7ad91bd53
Punar Randhawa <prandhawa2005@outlook.com>
2026-04-05 12:58:09 -0600

    backend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,26 @@
 from fastapi import FastApi
+import mysql.connector
+
 
 app = FastApi()
+conn = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "Punar_19"
+)
+
+cursor = conn.cursor()
+
+@app.post("/register")
+def register(first_name:str, last_name:str, courses: str, year:int, home_address:str, course_num:int, email:str):
+    sql = "INSERT INTO user()"
+    values = ""
+
+    cursor.execute(sql,values)
+    conn.commit()
+
+
+@app.post("/token")
+
+
 
